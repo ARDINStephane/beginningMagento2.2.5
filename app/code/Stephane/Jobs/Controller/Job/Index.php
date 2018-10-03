563b5e8aaeac51d534b572079b8e517ccabb5897
Stephane Ardin <stephane.ardin@coding-academy.fr>
2018-10-03 15:01:51 +0200

    if (->_jobCollection === null) {

---
+++ app/code/Stephane/Jobs/Controller/Job/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Stephane\Jobs\Controller\Job;
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $this->_view->getLayout()->initMessages();
+        $this->_view->renderLayout();
+    }
+}
--- app/code/Stephane/Jobs/Model/ResourceModel/Department.php
+++ app/code/Stephane/Jobs/Model/ResourceModel/Department.php
@@ -8,7 +8,6 @@ use \Magento\Framework\Model\ResourceModel\Db\AbstractDb;
  */
 class Department extends AbstractDb
 {
-
     /**
      * Initialize resource model
      *
